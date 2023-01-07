fc347368211d63a0b788625f098efd50cdc20062
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-01-07 13:39:30 +0530

    Update code.py
    Basic corrections were done.

--- code.py
+++ code.py
@@ -5,4 +5,4 @@ print(name("Nitkarsh Chourasia"))
 
 # Do add codes as you like. 
 # Edit code in whatever way you like, just keep in mind to preserve all the priviously entered details or functionalities by other users.
-# That is the only constraint to keep in mind which adding your preferred lines of logics.
+# That is the only constraint to keep in mind while adding your preferred lines of logics.
