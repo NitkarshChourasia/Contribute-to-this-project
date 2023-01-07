7163fdb5237334d5062226a9ecd4e821b598fd83
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-01-07 13:38:46 +0530

    Create code.py
    Just creating the basic template of this file to get started.

---
+++ code.py
@@ -0,0 +1,8 @@
+def name(nameInput):
+  return nameInput
+  
+print(name("Nitkarsh Chourasia"))
+
+# Do add codes as you like. 
+# Edit code in whatever way you like, just keep in mind to preserve all the priviously entered details or functionalities by other users.
+# That is the only constraint to keep in mind which adding your preferred lines of logics.
