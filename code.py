522813335683aaa39f396ef934d5952cfb5f3ece
T K <81703257+iknowtalha@users.noreply.github.com>
2023-02-08 15:59:47 +0500

    New Commit

--- code.py
+++ code.py
@@ -2,6 +2,7 @@ def name(nameInput):
   return nameInput
   
 print(name("Nitkarsh Chourasia"))
+print(name("iknowtalha"))
 
 # Do add codes as you like. 
 # Edit code in whatever way you like, just keep in mind to preserve all the priviously entered details or functionalities by other users.
